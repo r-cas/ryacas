cc7df0348a6181c302b80f8227d823ad666669e0
ggrothendieck <ggrothendieck@d6cf4d81-3a1d-0410-b626-8d9e83342ebe>
2006-09-19 14:35:31 +0000

    pass ... to yacas in print.Expr and print.Sym, added Exprq, fixed bug in Math.Expr, added to WISHLIST

--- trunk/R/Expr.R
+++ trunk/R/Expr.R
@@ -1,4 +1,5 @@
 Expr <- function(x) structure(as.expression(x), class = c("Expr", "expression"))
+Exprq <- function(x) Expr(substitute(x))
 
 as.character.Expr <- function(x, ...) as.character(unclass(x))
 
@@ -15,14 +16,14 @@ Math.Expr <- function(x) {
 	idx <- match(.Generic, transtab[,1], nomatch = 0)
 	fn <- if (idx > 0) transtab[idx, 3] else .Generic
 	L <- c(fn = as.name(fn), as.list(match.call())[-1])
-	substitute(fn(x), L)
+	Expr(substitute(fn(x), L))
 }
 
 
-deriv.Expr <- function(expr, name = x, n = 1, ...)
+deriv.Expr <- function(expr, name = Exprq(x), n = 1, ...)
 	Expr(substitute(deriv(expr,name,n), as.list(match.call())[-1]))
 
-print.Expr <- function(x, ...) print(yacas(x))
+print.Expr <- function(x, ...) print(yacas(x, ...))
 
 # test
 # x <- Expr(expression(x))
