73d608e32edd816f3a6bf184dafca1f381208bbe
ggrothendieck <ggrothendieck@d6cf4d81-3a1d-0410-b626-8d9e83342ebe>
2007-01-10 01:17:24 +0000

    reworked yacasInstall to download .zip file rather than yacas.exe and scripts.dat separately.  Eliminated yacasCheck.

--- trunk/R/zzz.R
+++ trunk/R/zzz.R
@@ -1,21 +1,12 @@
 
 .First.lib <- function(lib, pkg, ...) {
    if (.Platform$OS.type != "windows") return()
-   # library.dynam("Ryacas", pkg, lib)
-   chk <- yacasCheck()
-   if (is.na(chk)) return()
-   if (chk == 1) cat("Wrong version of yacas installed.\n")
-   if (chk == -1) cat("yacas not found.\n")
-   if (Sys.getenv("YACAS_INVOKE_STRING") == "") {
-         if (is.na(chk)) return()
-         if (chk != 0) {
-              urlbase <- "http://ryacas.googlecode.com/svn/trunk/inst/yacdir"
-              cat("Run yacasInstall() without arguments to install yacas\n")
-	      cat(" from", file.path(urlbase, "yacas.exe"), "\n")
-	      cat(" and", file.path(urlbase, "scripts.dat"), "\n")
-	      cat(" to", yacasFile("yacas.exe"), "and", 
-                yacasFile("scripts.dat"), "\n")
-          }
+
+   if (!file.exists(yacasFile("yacas.exe")) ||  
+       !file.exists(yacasFile("scripts.dat"))) {
+	cat(yacasFile("yacas.exe"), "or", yacasFile("scripts.dat"), 
+           "not found.\n")
+        cat("Run yacasInstall() without arguments to install yacas.\n")
    }
    invisible()
 }
