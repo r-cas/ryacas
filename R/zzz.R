1db0c4f7799c3f19ce618c1bee7323bfefa41ecf
ggrothendieck <ggrothendieck@d6cf4d81-3a1d-0410-b626-8d9e83342ebe>
2007-01-10 01:40:48 +0000

    The [[1]] output by print.yacas has been removed since print.Sym calls yacas.print and it seemed confusing on print.Sym.

--- R/zzz.R
+++ R/zzz.R
@@ -4,8 +4,8 @@
 
    if (!file.exists(yacasFile("yacas.exe")) ||  
        !file.exists(yacasFile("scripts.dat"))) {
-	cat(yacasFile("yacas.exe"), "or", yacasFile("scripts.dat"), 
-           "not found.\n")
+	cat(yacasFile("yacas.exe"), "\n   or", yacasFile("scripts.dat"), 
+           "\n   not found.\n")
         cat("Run yacasInstall() without arguments to install yacas.\n")
    }
    invisible()
