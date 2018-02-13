ce54df5ce574d5c9a41cbf7b5c18d76e9c73e674
Hao <32727671+HaoLi111@users.noreply.github.com>
2018-02-14 12:09:55 +1300

    Update Sym2.R

--- R/Sym2.R
+++ R/Sym2.R
@@ -19,13 +19,14 @@ Sym<-function (...)
 {
   if(!is.character(...)){
     assign(deparse(substitute(...)),Sym(paste(as.character(deparse(substitute(...))),'s',sep = '')),pos = .GlobalEnv)
-  }
+  }else{
   args <- list(...)
   value <- if (length(args) > 1) 
     paste("(", ..., ")")
   else paste(args[[1]])
   class(value) <- c("Sym", "character")
   value
+  }
 }
 
 as.character.Sym <- function(x, ...) as.character(unclass(x))
