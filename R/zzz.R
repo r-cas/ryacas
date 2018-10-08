9229898ce14d3a6a444826e96e61efe16b119168
Mikkel Meyer Andersen <mikl@mikl.dk>
2018-10-08 13:51:01 +0200

    onLoad had cat instead of packageStartupMessage

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 .onLoad <- function(libname, pkgname){
   if (exists("ryacas_devel_use_devtools", envir = .GlobalEnv)) {
-    cat("Initializing internal Yacas... ")  
-    # Force initialize so that a new instance is created.
+    packageStartupMessage("Initialising internal Yacas... ", appendLF = FALSE)  
+    # Force initialise so that a new instance is created.
     # Especially useful during development with e.g. devtools::load_all().
     yacas_init_force()
-    cat("Done.\n")
+    packageStartupMessage("Done.", appendLF = TRUE)
   }
 }
 
