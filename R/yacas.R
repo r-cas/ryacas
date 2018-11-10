14b40fc07ff52161ef29da4bd9d6db05262fee7c
Mikkel Meyer Andersen <mikl@mikl.dk>
2018-11-10 10:24:49 +0100

    Minor fixes -- again

--- R/yacas.R
+++ R/yacas.R
@@ -42,7 +42,7 @@
 #' information.
 #' @usage 
 #' yacas(x, ...)
-#' yacas(x, verbose = FALSE, method, retclass = c("expression", "character", "unquote"), addSemi = TRUE, ...)
+#' yacas.character(x, verbose = FALSE, method, retclass = c("expression", "character", "unquote"), addSemi = TRUE, ...)
 #' @note Windows Installation.  On Windows one can install \code{Ryacas} by
 #' issuing the commands:
 #' 
