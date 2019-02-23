6fc0edd7c898a00086e527547dff5f2f8bd4f8a4
Mikkel Meyer Andersen <mikl@mikl.dk>
2019-02-23 20:16:20 +0100

    Trying to fix yacmode() so that yacas assignment works

--- R/yacmode.R
+++ R/yacmode.R
@@ -29,22 +29,26 @@ yacmode <-function (){
     while (length(which(c("stop;", "stop", "end;", "end", "quit;",
                           "quit", "exit;", "exit", "e;", "e", "q;", "q","\n") == x)) ==
            0) {
+
+      o <- yacas(x)
+      print(o)
       
-      x <- gsub("Out>","#",x)
-      x <- gsub(" ","",unlist(strsplit(x,"#"))[1])
-      x <- gsub(" ","",gsub("In>",'',x))
-      if (x != '' && !is.na(x)){
-        pr <- try( parse(text = x ),silent=TRUE)
-        if (class(pr)=='try-error'){
-          # o<- yacas(x,retclass='character');
-          o<- yacas(x)
-        } else {
-          o <- yacas(parse(text = x)); 
-        }
-        # o <- paste(o)
-        # a<-lapply(o,function(s) cat(paste(s,"\n")))
-        print(o)
-      }    
+      # x <- gsub("Out>","#",x)
+      # x <- gsub(" ","",unlist(strsplit(x,"#"))[1])
+      # x <- gsub(" ","",gsub("In>",'',x))
+      # if (x != '' && !is.na(x)){
+      #   pr <- try( parse(text = x ),silent=TRUE)
+      #   if (class(pr)=='try-error'){
+      #     # o<- yacas(x,retclass='character');
+      #     o<- yacas(x)
+      #   } else {
+      #     o <- yacas(parse(text = x)); 
+      #   }
+      #   # o <- paste(o)
+      #   # a<-lapply(o,function(s) cat(paste(s,"\n")))
+      #   print(o)
+      # }
+      
       x <- readline("Yacas->")
     }
   }
