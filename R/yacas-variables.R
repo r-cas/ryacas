48291148950279bc90d26bd41c1fabfd74c54034
Mikkel Meyer Andersen <mikl@mikl.dk>
2019-03-27 00:59:00 +0100

    ls_yacas() to y_ls()

--- R/yacas-variables.R
+++ R/yacas-variables.R
@@ -1,7 +1,7 @@
 #' Get Yacas variables
 #' 
 #' @export
-ls_yacas <- function() {
+y_ls <- function() {
   x <- yacas("Variables()", addSemi = FALSE, retclass = "character")
   z <- x$LinAlgForm
   z <- setdiff(z, "i") # i = imaginary unit: I in Yacas, i in R
