650bb341db2e5fe25799a8ae74d03fc8ec86d890
Paolo Bosetti <paolo.bosetti@unitn.it>
2022-10-27 14:19:47 +0200

    Added y_eval to NAMESPACE

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // yacas_init_force_path
 void yacas_init_force_path(std::string path, bool ryacas_init);
 RcppExport SEXP _Ryacas_yacas_init_force_path(SEXP pathSEXP, SEXP ryacas_initSEXP) {
